c6312cf15fa05c03f8f2c695d35a865697001481
sayantanthessatan009 <sayantan.achraya.777@gmail.com>
2025-08-19 22:29:03 +0530

    .json

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/EMAIL-DASH-19-08-25/',   // 👈 EXACT repo name
+  base: '/',   // 👈 EXACT repo name
 })
